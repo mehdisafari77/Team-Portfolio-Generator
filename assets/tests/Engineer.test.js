2efb3ba118f0bd1356f63f0851a7ded10dab0745
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-16 21:53:51 -0700

    fix engineer tests

--- assets/tests/Engineer.test.js
+++ assets/tests/Engineer.test.js
@@ -1,19 +1,20 @@
+const { getMaxListeners } = require("process")
 const Engineer = require("../lib/Engineer")
 
 test("Can set Github account", () => {
     const testUser = "GithubUser"
-    const newEngineer = new Engineer("mehdi", 1, testUser)
-    expect(newEngineer.name).toBe(testUser)
+    const newEngineer = new Engineer("mehdi", 1, "mehdi@gmail.com", testUser)
+    expect(newEngineer.github).toBe(testUser)
 })
 
-test("getRole() return \"Employee\"", () => {
-    const testEmployee = "Employee"
-    const newEmployee = new Engineer("mehdi", 1, "mehdi@gmail.com")
-    expect(newEmployee.getRole()).toBe(testEmployee)
+test("getRole() return \"Engineer\"", () => {
+    const testEngineer = "Engineer"
+    const newEngineer = new Engineer("mehdi", 1, "mehdi@gmail.com", "GithubUser")
+    expect(newEngineer.getRole()).toBe(testEngineer)
 })
 
 test("getGithub() gets Github username ", () => {
     const testUser = "GithubUser"
-    const newEngineer = new Engineer("mehdi", 1, testUser)
+    const newEngineer = new Engineer("mehdi", 1, "mehdi@gmail.com", testUser)
     expect(newEngineer.getGithub()).toBe(testUser)
 })
