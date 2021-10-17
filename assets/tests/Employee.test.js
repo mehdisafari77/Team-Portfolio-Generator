ef229fabea51d3648c1ec0371a89961568d0bf73
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-16 20:09:16 -0700

    add 3 new tests to employee test file

--- assets/tests/Employee.test.js
+++ assets/tests/Employee.test.js
@@ -6,8 +6,26 @@ test("Can instantiate Employee instance", () => {
     expect(typeof(newEmployee)).toBe("object")
 })
 
-test("Can set name with constructors", () => {
+test("Can set name", () => {
     const name = "mehdi"
     const newEmployee = new Employee(name)
     expect(newEmployee.name).toBe(name)
+})
+
+test("Can set email", () => {
+    const testEmail = "mehdi@gmail.com"
+    const newEmployee = new Employee("mehdi", 1, testEmail)
+    expect(newEmployee.email).toBe(testEmail)
+})
+
+test("Can set id", () => {
+    const testId = 1234
+    const newEmployee = new Employee("mehdi", testId)
+    expect(newEmployee.id).toBe(testId)
+})
+
+test("Can set id", () => {
+    const testId = 1234
+    const newEmployee = new Employee("mehdi", testId)
+    expect(newEmployee.id).toBe(testId)
 })
